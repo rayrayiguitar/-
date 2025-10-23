0adf092a52966278b3bd03c31bfb0e75606b1822
rayrayiguitar <thank87815@gmail.com>
2025-10-23 15:06:06 +0800

    Delete 20251023 directory

--- 20251023/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const btn = document.getElementById("thanksBtn");
-    const msg = document.getElementById("message");
-
-    btn.addEventListener("click", function() {
-        msg.classList.toggle("show");
-    });
-});
